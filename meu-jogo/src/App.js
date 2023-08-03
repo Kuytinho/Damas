9825660a4604af58b72995793ed56810e61d270b
Lucas Carvalho <drigerm@hotmail.com>
2023-08-03 12:24:37 -0300

    Router configurado/rota home/rota not found

---
+++ meu-jogo/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={ <Home /> }/>
+        <Route path="*" element={<NotFound />} /> 
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
